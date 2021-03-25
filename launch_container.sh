debff971bd3d68a0055ff554692f73b10d6eaac0
Jack S. Hale <mail@jackhale.co.uk>
2021-03-25 16:02:52 +0100

    Start in right directory.

--- launch_container.sh
+++ launch_container.sh
@@ -2,5 +2,5 @@
 # docker pull dolfinx/lab
 # docker stop dolfinx-newfrac
 # docker rm dolfinx-newfrac
-docker run --init -p 8888:8888 --name dolfinx-newfrac -v "$(pwd)":/root/shared dolfinx/lab
+docker run --init -p 8888:8888 --name dolfinx-newfrac -v "$(pwd)":/root/shared -w /root/shared dolfinx/lab
 
